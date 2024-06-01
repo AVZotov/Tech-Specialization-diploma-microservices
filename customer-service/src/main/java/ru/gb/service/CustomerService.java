273ac9f69b14bf482c1003c74ee6cfd33e232c40
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-06-01 19:14:27 +0300

    Eureka server implemented and tested

--- customer-service/src/main/java/ru/gb/service/CustomerService.java
+++ customer-service/src/main/java/ru/gb/service/CustomerService.java
@@ -26,7 +26,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-checks/{customerId}",
+                "http://FRAUD/api/v1/fraud-checks/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
